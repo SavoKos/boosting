def51fd2c702786b28a51490c0ed5c98cfac74dd
Savo Kos <savo.kos.sk@gmail.com>
2024-04-21 19:17:08 +0200

    Added netwins page

--- app/components/netwinsLogic.jsx
+++ app/components/netwinsLogic.jsx
@@ -1,45 +1,34 @@
 'use client';
 
-import { useEffect, useState } from 'react';
 import { IoOptionsOutline } from 'react-icons/io5';
 import Switch from '../components/switch';
 import { FaSackDollar } from 'react-icons/fa6';
+import { useEffect, useState } from 'react';
 
-export default function Home() {
-  const lpGain1Prices = [
-    4.04, 4.04, 4.04, 4.04, 4.56, 4.56, 4.56, 5.17, 5.42, 5.77, 6.03, 8.36,
-    9.05, 9.74, 11.63, 12.5, 13.36, 15.35, 17.24, 21.98, 23.11, 24.4, 26.73,
-    27.85, 37.34, 48.72, 66.66, 80.63,
-  ];
-  const lpGain2Prices = [
-    3.52, 3.52, 3.52, 3.52, 3.97, 3.97, 4.04, 4.49, 4.72, 5.02, 5.24, 7.27,
-    7.87, 8.47, 10.12, 10.87, 11.62, 13.34, 14.99, 19.12, 20.09, 21.22, 23.24,
-    24.22, 32.47, 42.37, 57.97, 70.12,
-  ];
-  const lpGain3Prices = [
-    3.52, 3.52, 3.52, 3.52, 3.97, 3.97, 4.04, 4.49, 4.72, 5.02, 5.24, 7.27,
-    7.87, 8.47, 10.12, 10.87, 11.62, 13.34, 14.99, 19.12, 20.09, 21.22, 23.24,
-    24.22, 32.47, 42.37, 57.97, 70.12,
-  ];
-
-  const [total, setTotal] = useState(0);
-  const [slicedPrices, setSlicedPrices] = useState(lpGain1Prices);
-  const [currentLeague, setCurrentLeague] = useState('0');
-  const [desiredLeague, setDesiredLeague] = useState('6');
-  const [currentDivision, setCurrentDivision] = useState('0');
-  const [desiredDivision, setDesiredDivision] = useState('0');
-  const [discount, setDiscount] = useState(1);
-  const [prices, setPrices] = useState(lpGain1Prices);
+export default function Netwins() {
+  const [numOfGames, setNumOfGames] = useState(1);
+  const [rank, setRank] = useState('5');
+  const [division, setDivision] = useState('0');
   const [additionalPercent, setAdditionalPercent] = useState(1);
+  const [total, setTotal] = useState(0);
+  const prices = [
+    0.89, 0.89, 0.89, 0.89, 0.92, 0.92, 0.92, 0.92, 1.04, 1.12, 1.57, 1.94,
+    2.32, 2.32, 2.62, 3.07, 3.74, 3.74, 4.04, 4.49, 4.57, 4.64, 4.79, 4.87,
+    5.24, 7.72, 9.44, 10.87, 21.65, 27.79, 33.07,
+  ];
+
+  console.log(rank, division);
+  const [slicedPrices, setSlicedPrices] = useState(prices);
 
   useEffect(() => {
-    setSlicedPrices(
-      [...prices].slice(
-        currentLeague * 4 + currentDivision * 1,
-        (desiredLeague * 1 + 1) * 4 - desiredDivision * 1 - 1
-      )
-    );
-  }, [currentLeague, currentDivision, desiredDivision, desiredLeague, prices]);
+    if (rank > 6)
+      return setSlicedPrices(
+        [...prices].slice(0, 7 * 4 + division * 1 + 1 + (rank * 1 - 7))
+      );
+
+    setSlicedPrices([...prices].slice(0, rank * 4 + division * 1 + 1));
+  }, [rank, division, numOfGames]);
+
   useEffect(() => {
     if (slicedPrices.length < 1) return;
 
@@ -47,19 +36,19 @@ export default function Home() {
       slicedPrices.reduce(
         (accumulator, currentValue) => accumulator + currentValue
       ) *
-        discount *
+        numOfGames *
         additionalPercent
     );
-  }, [slicedPrices, discount, additionalPercent]);
+  }, [slicedPrices, numOfGames, additionalPercent]);
 
   return (
     <div className='flex flex-col md:flex-row max-w-screen-xl m-auto'>
-      <div className='flex lg:flex-wrap md:w-1/2 flex-col lg:flex-row'>
-        <div className='bg-pink rounded-2xl p-5 lg:flex-[0_0_50%] border-primary-blue border-4'>
+      <div className='flex md:w-1/2 flex-col lg:flex-row'>
+        <div className='bg-pink rounded-2xl p-5 border-primary-blue border-4 h-fit w-1/2'>
           <div className='flex items-center gap-3 mb-5'>
             <div className='bg-primary-blue rounded-lg w-8 h-8'></div>
             <h3 className='text-white text-lg lg:text-[19px] font-medium leading-[1em] '>
-              Current League
+              Rank
             </h3>
           </div>
           <div>
@@ -68,7 +57,8 @@ export default function Home() {
               name=''
               id=''
               onChange={(e) => {
-                setCurrentLeague(e.target.value);
+                if (e.target.value > 6) setDivision(0);
+                setRank(e.target.value);
               }}
             >
               <option className='font-light' value='0'>
@@ -86,30 +76,20 @@ export default function Home() {
               <option className='font-light' value='4'>
                 Platinum
               </option>
-              <option className='font-light' value='5'>
+              <option className='font-light' value='5' selected>
                 Emerald
               </option>
               <option className='font-light' value='6'>
                 Diamond
               </option>
-            </select>
-            <select
-              className='block w-full max-w-64 m-auto mt-3 p-3 text-base font-ReadexPro font-light'
-              name=''
-              id=''
-              onChange={(e) => setCurrentDivision(e.target.value)}
-            >
-              <option className='font-light' name='' id='' value='0'>
-                Division IV
+              <option className='font-light' value='7'>
+                Master
               </option>
-              <option className='font-light' name='' id='' value='1'>
-                Division III
+              <option className='font-light' value='8'>
+                Grandmaster
               </option>
-              <option className='font-light' name='' id='' value='2'>
-                Division II
-              </option>
-              <option className='font-light' name='' id='' value='3'>
-                Division I
+              <option className='font-light' value='9'>
+                Challenger
               </option>
             </select>
           </div>
@@ -117,7 +97,7 @@ export default function Home() {
             <div className='flex items-center gap-3 my-5'>
               <div className='bg-primary-blue rounded-lg w-8 h-8'></div>
               <h3 className='text-white text-lg lg:text-[19px] font-medium leading-[1em] '>
-                Current LP
+                Division
               </h3>
             </div>
             <div>
@@ -125,118 +105,51 @@ export default function Home() {
                 className='block w-full max-w-64 m-auto mt-3 p-3 text-base font-ReadexPro font-light'
                 name=''
                 id=''
-                onChange={(e) => setDiscount(1 - e.target.value)}
+                onChange={(e) => {
+                  setDivision(e.target.value);
+                }}
               >
-                <option className='font-light' value='0'>
-                  0-20
-                </option>
-                <option className='font-light' value='0.1699'>
-                  21-40
-                </option>
-                <option className='font-light' value='0.1868'>
-                  41-60
-                </option>
-                <option className='font-light' value='0.2369'>
-                  61-80
-                </option>
-                <option className='font-light' value='0.3107'>
-                  81-100
-                </option>
+                {Number(rank) <= 6 && (
+                  <>
+                    <option
+                      className='font-light'
+                      name=''
+                      id=''
+                      value='0'
+                      selected
+                    >
+                      Division IV
+                    </option>
+                    <option className='font-light' name='' id='' value='1'>
+                      Division III
+                    </option>
+                    <option className='font-light' name='' id='' value='2'>
+                      Division II
+                    </option>
+                    <option className='font-light' name='' id='' value='3'>
+                      Division I
+                    </option>
+                  </>
+                )}
+                {Number(rank) > 6 && (
+                  <option
+                    className='font-light'
+                    name=''
+                    id=''
+                    value='0'
+                    selected
+                  >
+                    Division I
+                  </option>
+                )}
               </select>
             </div>
           </div>
         </div>
-        <div className='bg-pink rounded-2xl p-5 lg:flex-[0_0_50%] border-primary-blue border-4'>
-          <div className='flex items-center gap-3 mb-5'>
-            <div className='bg-primary-blue rounded-lg w-8 h-8'></div>
-            <h3 className='text-white text-lg lg:text-[19px] font-medium leading-[1em] '>
-              Desired League
-            </h3>
-          </div>
-          <div>
-            <select
-              className='block w-full max-w-64 m-auto mt-3 p-3 text-base font-ReadexPro font-light'
-              name=''
-              id=''
-              onChange={(e) => {
-                setDesiredLeague(e.target.value);
-              }}
-            >
-              <option className='font-light' value='0'>
-                Iron
-              </option>
-              <option className='font-light' value='1'>
-                Bronze
-              </option>
-              <option className='font-light' value='2'>
-                Silver
-              </option>
-              <option className='font-light' value='3'>
-                Gold
-              </option>
-              <option className='font-light' value='4'>
-                Platinum
-              </option>
-              <option className='font-light' value='5'>
-                Emerald
-              </option>
-              <option className='font-light' value='6' selected>
-                Diamond
-              </option>
-            </select>
-            <select
-              className='block w-full max-w-64 m-auto mt-3 p-3 text-base font-ReadexPro font-light'
-              name=''
-              id=''
-              onChange={(e) => setDesiredDivision(e.target.value)}
-            >
-              <option className='font-light' name='' id='' value='3'>
-                Division IV
-              </option>
-              <option className='font-light' name='' id='' value='2'>
-                Division III
-              </option>
-              <option className='font-light' name='' id='' value='1'>
-                Division II
-              </option>
-              <option className='font-light' name='' id='' value='0' selected>
-                Division I
-              </option>
-            </select>
-          </div>
-          <div>
-            <div className='flex items-center gap-3 my-5'>
-              <div className='bg-primary-blue rounded-lg w-8 h-8'></div>
-              <h3 className='text-white text-lg lg:text-[19px] font-medium leading-[1em] '>
-                LP GAIN
-              </h3>
-            </div>
-            <select
-              className='block w-full max-w-64 m-auto mt-3 p-3 text-base font-ReadexPro font-light'
-              name=''
-              id=''
-              onChange={(e) => {
-                const { value } = e.target;
-                if (value == 1) setPrices(lpGain1Prices);
-                if (value == 2) setPrices(lpGain2Prices);
-                if (value == 3) setPrices(lpGain3Prices);
-              }}
-            >
-              <option className='font-light' value='1'>
-                10-19
-              </option>
-              <option className='font-light' value='2'>
-                20-24
-              </option>
-              <option className='font-light' value='3'>
-                25+
-              </option>
-            </select>
-          </div>
-        </div>
-        <div className='bg-pink rounded-2xl p-5 lg:flex-[0_0_100%] flex-col lg:flex-row flex border-primary-blue border-4 h-fit'>
-          <div className='w-full lg:w-1/2'>
-            <div className='flex items-center gap-3 mb-5'>
+
+        <div className='bg-pink rounded-2xl p-5 flex-col flex border-primary-blue border-4 h-fit w-1/2 gap-5'>
+          <div className='w-full '>
+            <div className='flex items-center gap-3 mb-5 '>
               <div className='bg-primary-blue rounded-lg w-8 h-8'></div>
               <h3 className='text-white text-lg lg:text-[19px] font-medium leading-[1em] '>
                 SERVER
@@ -246,25 +159,20 @@ export default function Home() {
               className='block w-full max-w-64 m-auto mt-3 p-3 text-base font-ReadexPro font-light'
               name=''
               id=''
-              onChange={(e) => {
-                const { value } = e.target;
-                if (value == 1) setPrices(lpGain1Prices);
-                if (value == 2) setPrices(lpGain2Prices);
-                if (value == 3) setPrices(lpGain3Prices);
-              }}
+              onChange={(e) => {}}
             >
               <option className='font-light' value='1'>
-                10-19
+                EU
               </option>
               <option className='font-light' value='2'>
-                20-24
+                ASIA
               </option>
               <option className='font-light' value='3'>
-                25+
+                AMERICA
               </option>
             </select>
           </div>
-          <div className='w-full lg:w-1/2'>
+          <div className='w-full'>
             <div className='flex items-center gap-3 mb-5'>
               <div className='bg-primary-blue rounded-lg w-8 h-8'></div>
               <h3 className='text-white text-lg lg:text-[19px] font-medium leading-[1em] '>
@@ -275,12 +183,7 @@ export default function Home() {
               className='block w-full max-w-64 m-auto mt-3 p-3 text-base font-ReadexPro font-light'
               name=''
               id=''
-              onChange={(e) => {
-                const { value } = e.target;
-                if (value == 1) setPrices(lpGain1Prices);
-                if (value == 2) setPrices(lpGain2Prices);
-                if (value == 3) setPrices(lpGain3Prices);
-              }}
+              onChange={(e) => {}}
             >
               <option className='font-light' value='1'>
                 10-19
@@ -290,6 +193,32 @@ export default function Home() {
               </option>
               <option className='font-light' value='3'>
                 25+
+              </option>
+            </select>
+          </div>
+          <div className='w-full'>
+            <div className='flex items-center gap-3 mb-5'>
+              <div className='bg-primary-blue rounded-lg w-8 h-8'></div>
+              <h3 className='text-white text-lg lg:text-[19px] font-medium leading-[1em] '>
+                NUMBER OF GAMES
+              </h3>
+            </div>
+            <select
+              className='block w-full max-w-64 m-auto mt-3 p-3 text-base font-ReadexPro font-light'
+              name=''
+              id=''
+              onChange={(e) => {
+                setNumOfGames(e.target.value * 1);
+              }}
+            >
+              <option className='font-light' value='1'>
+                1
+              </option>
+              <option className='font-light' value='2'>
+                2
+              </option>
+              <option className='font-light' value='3'>
+                3
               </option>
             </select>
           </div>
@@ -366,5 +295,3 @@ export default function Home() {
     </div>
   );
 }
-
-/*  */
---
+++ app/netwins/page.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import Navigation from '../Navigation';
+import Boosting from '../../app/components/boosting';
+import bg from './../../public/bg.png';
+import Netwins from '../components/netwinsLogic';
+
+function page() {
+  return (
+    <div
+      className='px-3.5 pb-20 bg-primary-blue relative overflow-x-hidden'
+      style={{
+        backgroundImage: `url(${bg.src})`,
+        width: '100%',
+        height: '100%',
+      }}
+    >
+      <Navigation active='netwins' />
+      <div className='bg-transparent bg-opacity-20 p-3 border-gray-700 border-[1px] rounded-md m-auto block bg-white outline-none border-none max-w-screen-lg w-full my-32'>
+        <div className='bg-gradient-to-r from-[#703ffe] to-[#b392ff] px-4 rounded-md text-white text-[35px] md:text-[45px] lg:text-5xl lg:font-medium lg:leading-[1.4em] w-full text-center py-10'>
+          Boosting
+        </div>
+      </div>
+      <div className='flex max-w-screen-lg w-full justify-between m-auto mb-32 flex-col gap-20 md:flex-row md:gap-0'>
+        <div className='flex flex-col items-center'>
+          <div className='w-28 h-28 bg-pink rounded-full'></div>
+          <p className='text-white text-lg lg:text-xl leading-[1em] font-medium mt-5'>
+            Division boosting
+          </p>
+        </div>
+        <div className='flex flex-col items-center'>
+          <div className='w-28 h-28 bg-pink rounded-full'></div>
+          <p className='text-white text-lg lg:text-xl leading-[1em] font-medium mt-5'>
+            Net Wins
+          </p>
+        </div>
+        <div className='flex flex-col items-center'>
+          <div className='w-28 h-28 bg-pink rounded-full'></div>
+          <p className='text-white text-lg lg:text-xl leading-[1em] font-medium mt-5'>
+            Placement Games
+          </p>
+        </div>
+      </div>
+      <Netwins />
+    </div>
+  );
+}
+
+export default page;
